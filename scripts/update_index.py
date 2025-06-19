20743077abc1feaf2e165e0657a38b8375d0d6b4
Riws <t.debruyne@protonmail.com>
2025-06-19 08:32:41 +0200

    feat: update_index create default page and language page

--- scripts/update_index.py
+++ scripts/update_index.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from os import sep as os_sep
 from pathlib import Path
@@ -7,6 +8,7 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 from scripts.utils import ModManager
 from settings import (
     LANGUAGE_CONFIG,
+    LANGUAGE_DEFAULT,
     CategoryEnum,
     GameEnum,
     attrs_icon_data,
@@ -14,11 +16,22 @@ from settings import (
     resize_image_from_width,
 )
 
-# TODO: Script pour générer le fichier en par défaut (avec prise en compte de la langue et retrait de team)
-# Réorienter automatiquement vers la page de sa langue
+# TODO:Réorienter automatiquement vers la page de sa langue
+
+logger = logging.getLogger(__name__)
 
 
 def main(**kwargs):
+    def build_html_page(static: str) -> str:
+        return env.get_template("base.html").render(
+            games=GameEnum,
+            categories=categories_mod,
+            static=static,
+            attrs_icon_data=attrs_icon_data,
+            mod_length=len(mods),
+            language=language,
+        )
+
     env = Environment(
         loader=PackageLoader("docs", "templates"),
         autoescape=select_autoescape(["html"]),
@@ -26,7 +39,6 @@ def main(**kwargs):
         lstrip_blocks=True,  # Supprime les espaces avant un bloc Jinja
     )
 
-    root = Path.cwd()
     resize_image_from_width(24)
 
     # auto-discover languages
@@ -47,16 +59,22 @@ def main(**kwargs):
             for mod in mods:
                 for category in mod.categories:
                     categories_mod[category].append(mod)
-            page_html = env.get_template("base.html").render(
-                games=GameEnum,
-                categories=categories_mod,
-                static=f"..{os_sep}static{os_sep}",
-                attrs_icon_data=attrs_icon_data,
-                mod_length=len(mods),
-                language=language,
-            )
 
-            dir_path = root / "docs" / language
-            dir_path.mkdir(parents=True, exist_ok=True)
-            with open(dir_path / "index.html", "w", encoding="utf-8") as f:
-                f.write(page_html)
+            page_html = build_html_page(static=f"..{os_sep}static{os_sep}")
+            create_page_language(page_html, language)
+
+            # on crée aussi la page par defaut (home), qui est celle du language par défaut
+            if language == LANGUAGE_DEFAULT:
+                page_html = build_html_page(static=f"static{os_sep}")
+                create_page_language(page_html, "")
+
+
+def create_page_language(page_html: str, language: str) -> None:
+    dir_path = Path.cwd() / "docs"
+    if language:
+        dir_path /= language
+    dir_path.mkdir(parents=True, exist_ok=True)
+
+    logger.info("Generating index page for %s", language)
+    with open(dir_path / "index.html", "w", encoding="utf-8") as f:
+        f.write(page_html)
