ec0323a1cb6e5fed125d5b920302ee7ad6119bb6
riws <t.debruyne@protonmail.com>
2026-01-13 07:21:44 +0100

    feat: add Tp2Cleaner.clean_languages

--- scripts/cleaner/tp2.py
+++ scripts/cleaner/tp2.py
@@ -2,4 +2,18 @@ from scripts.cleaner.utils import ModCleaner
 
 
 class Tp2Cleaner(ModCleaner):
-    pass
+    def clean_languages(self) -> list[str]:
+        if not self.data.get("languages"):
+            return list()
+
+        languages = set()
+        for lang_name, lang_dir_name in zip(
+            self.data["languages"][::2], self.data["languages"][1::2], strict=True
+        ):
+            cleaned_lang = self._clean_lang(lang_dir_name)
+            if not cleaned_lang:
+                cleaned_lang = self._clean_lang(lang_name)
+            if cleaned_lang:
+                languages.add(cleaned_lang)
+
+        return sorted(languages)
