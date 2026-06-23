e0608a331b8c91805331a580e72eaadc4f556d1a
riws <t.debruyne@protonmail.com>
2026-06-23 11:12:32 +0200

    fix: dont simplify gh raw urls

--- scripts/utils.py
+++ scripts/utils.py
@@ -151,9 +151,12 @@ class CleanModMixin:
 
 
 def simplify_url(url: str) -> str:
-    if url.startswith(
-        ("https://github.com/", "https://forums.beamdog.com/discussion/")
-    ) and not url.startswith("https://forums.beamdog.com/discussion/comment/"):
+    if (
+        url.startswith("https://github.com")
+        and "raw/refs/heads/" not in url
+        or url.startswith("https://forums.beamdog.com/discussion/")
+        and not url.startswith("https://forums.beamdog.com/discussion/comment/")
+    ):
         url = "/".join(url.split("/")[:5])
     return url.removesuffix("/")
 
